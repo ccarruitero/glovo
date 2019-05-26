024f6dd92f9591fa034ce502b6f67e0220e5cd96
César Carruitero <ccarruitero@protonmail.com>
2019-05-26 16:49:07 -0500

    add missing Glovo endpoints

--- lib/glovo/client.rb
+++ lib/glovo/client.rb
@@ -5,6 +5,9 @@ require 'glovo/configuration'
 
 module Glovo
   class Client
+    # Returns the characteristics of Glovo's working areas.
+    # This data will be used to check for valid pickup and delivery locations,
+    # and times
     def working_areas
       res = request('get', "https://#{host}/b2b/working-areas")
 
@@ -14,6 +17,49 @@ module Glovo
         parse_response res
       end
     end
+
+    # Provide a price estimation for an order.
+    # The response amount will always be in the minor of the currency
+    # (e.g. cents for EUR).
+    def estimate_cost(order_params)
+      res = request('post', "https://#{host}/b2b/orders/estimate", order_params)
+      parse_response res
+    end
+
+    # Create a scheduled or immediate order.
+    # If you want to schedule an order, provide a scheduleTime. If you don't,
+    # it'll be immediately activated for deliver.
+    def create_order(order_params)
+      res = request('post', "https://#{host}/b2b/orders", order_params)
+      parse_response res
+    end
+
+    # Retrieve information about a single order.
+    def get_order(order_id)
+      res = request('get', "https://#{host}/b2b/orders/#{order_id}")
+      parse_response res
+    end
+
+    # Return the position (latitude, longitude) of the courier.
+    def track_order(order_id)
+      res = request('get', "https://#{host}/b2b/orders/#{order_id}/tracking")
+      parse_response res
+    end
+
+    # Name and contact phone of the courier if the order is active.
+    # Error if the order is not active.
+    def order_courier(order_id)
+      res = request('get', "https://#{host}/b2b/orders/#{order_id}/courier-contact")
+      parse_response res
+    end
+
+    # Cancel a scheduled order. Active orders cannot be canceled.
+    def cancel_order(order_id)
+      res = request('post', "https://#{host}/b2b/orders/#{order_id}/cancel")
+      parse_response res
+    end
+
+    private
 
     def host
       prefix = %w[sandbox test].include?(Glovo.configuration&.env) ? 'stage' : ''
@@ -28,8 +74,8 @@ module Glovo
       }
     end
 
-    def request(method, url)
-      HTTParty.send(method, url, headers: headers)
+    def request(method, url, params = {})
+      HTTParty.send(method, url, headers: headers, body: params)
     end
 
     def parse_response(res)
